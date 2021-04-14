91692f27a85e8f5dbaf924ec6579b0ffde69263e
R0NALD117 <79949289+R0NALD117@users.noreply.github.com>
2021-04-14 13:11:44 -0500

    Upddate

--- practica1/Calculadora.java
+++ practica1/Calculadora.java
@@ -1,13 +1,15 @@
-package practica1;
+package PracticasTAP.practica1;
 
 import javax.swing.JFrame;
 
 public class Calculadora {
-    public static void main(String args[]){
-
-        CalculadoraGUI test1 = new CalculadoraGUI("Calculadora Simple");
-
-        test1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	
+	public static void main(String args[]) {
+		
+		CalculadoraGUI test1 = new CalculadoraGUI("Calculadora Simple");
+		
+		test1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
     }
 
 }
--- practica1/CalculadoraGUI.java
+++ practica1/CalculadoraGUI.java
@@ -1,4 +1,4 @@
-package practica1;
+package PracticasTAP.practica1;
 
 import javax.swing.*;
 import java.awt.*;
