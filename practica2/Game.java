10ef40f049ac785fe43d8306a83c00d34424c31c
R0NALD117 <79949289+R0NALD117@users.noreply.github.com>
2021-04-16 21:30:48 -0500

    Update

---
+++ practica2/Game.java
@@ -0,0 +1,63 @@
+package practica2;
+
+import java.awt.*;
+
+public class Game extends Canvas {
+
+	private int x, y, w, h;
+    private boolean isClicked;
+    
+    public Game() {
+    	x = 30;
+        y = 30;
+        isClicked = false;
+    }
+	
+    public void paint(Graphics g) {
+    	
+    	g.setColor(Color.GREEN);
+    	g.fillOval(300, 300, 40, 40);
+    	g.setColor(Color.RED);
+    	g.fillRect(x, y, w, h);
+    }
+    
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getW() {
+        return w;
+    }
+
+    public void setW(int w) {
+        this.w = w;
+    }
+
+    public int getH() {
+        return h;
+    }
+
+    public void setH(int h) {
+        this.h = h;
+    }
+
+    public boolean isClicked() {
+        return isClicked;
+    }
+
+    public void setClicked() {
+        this.isClicked = !isClicked;
+    }
+}
---
+++ practica2/GameMain.java
@@ -0,0 +1,120 @@
+package practica2;
+
+import java.awt.*;
+import java.awt.event.*;
+
+public class GameMain extends Frame implements MouseListener, MouseMotionListener, KeyListener{
+	
+	private Game obj_pintable;
+	
+	public GameMain() {
+		
+		initComponent();
+	}
+	
+	public void initComponent() {
+		this.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+		});
+		obj_pintable = new Game();
+		obj_pintable.addMouseListener(this);
+        obj_pintable.addMouseMotionListener(this);
+        obj_pintable.addKeyListener(this);
+        this.addKeyListener(this);
+        this.add(obj_pintable);
+        
+        this.setSize(500,500);
+        this.setVisible(true);
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		GameMain test = new GameMain();
+	}
+
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		
+	}
+
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Esbozo de método generado automáticamente
+		
+	}
+
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Esbozo de método generado automáticamente
+		
+	}
+
+
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		
+		if(!obj_pintable.isClicked()){
+            obj_pintable.setX(e.getX());
+            obj_pintable.setY(e.getY());
+            obj_pintable.repaint();
+        }
+		
+	}
+
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		// TODO Esbozo de método generado automáticamente
+		
+	}
+
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		if(obj_pintable.isClicked()){
+            obj_pintable.setW(e.getX());
+            obj_pintable.setH(e.getY());
+            obj_pintable.repaint();
+        } else{
+            obj_pintable.setX(e.getX());
+            obj_pintable.setY(e.getY());
+        }
+        obj_pintable.setClicked();
+		
+	}
+
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Esbozo de método generado automáticamente
+		
+	}
+
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Esbozo de método generado automáticamente
+		
+	}
+
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Esbozo de método generado automáticamente
+		
+	}
+
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Esbozo de método generado automáticamente
+		
+	}
+
+}
